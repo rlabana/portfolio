ae0eef468af579d15e2dfaf4c5f1e34870421333
rlabana <labana_rk@hotmail.com>
2017-05-21 21:27:21 -0400

    Web page updated with new layout

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ALESX' });
-});
-//get is a method
-// so get can be changed to
-// post is usually works with JSON
-router.post('/alex', function (req, res, next){
-
-});
-
-
-
-router.get('/about', function(req, res, next) {
-    res.render('about', {title: 'About Me', name: 'Retinder Labana'});
+  res.render('index', { title: 'Retinder Labana' });
 });
 
 module.exports = router;
